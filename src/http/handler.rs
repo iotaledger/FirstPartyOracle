667f2250ad42628d4847d46c205502f9afb6a0eb
DyrellC <dyrell.chapman@iota.org>
2021-02-04 18:25:14 -0700

    Cors on POST/GET responses

--- src/http/handler.rs
+++ src/http/handler.rs
@@ -172,6 +172,9 @@ pub async fn retrieve_messages(
 fn respond(status: StatusCode, msg: String) -> Result<Response<Body>> {
     Ok(Response::builder()
         .status(status)
+        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Methods", "GET, POST")
+        .header("Access-Control-Allow-Headers", "*")
         .header(CONTENT_TYPE, "application/json")
         .body(Body::from(msg)).unwrap())
 }
